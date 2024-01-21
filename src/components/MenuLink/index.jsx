7adfcff195af8a289b3afa04b7190e5cf86f7966
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-01-21 15:55:31 -0300

    feat: criação do menulink
    criação do menu link com testes e storybook

---
+++ src/components/MenuLink/index.jsx
@@ -0,0 +1,20 @@
+import P from 'prop-types'
+
+import * as Styled from './styles'
+
+const MenuLink = ({ children, link, newTab = false }) => {
+  const target = newTab ? '_blank' : '_self'
+  return (
+    <Styled.Container href={link} target={target}>
+      {children}
+    </Styled.Container>
+  )
+}
+
+MenuLink.propTypes = {
+  children: P.node.isRequired,
+  link: P.string.isRequired,
+  newTab: P.bool,
+}
+
+export default MenuLink
