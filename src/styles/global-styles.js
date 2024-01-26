905fec22110167d09d5548894bdfc546869f7dc5
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-01-26 17:50:26 -0300

    feat: criado componente GridContent
    criados estilos, story e testes do componente GridContent

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -18,6 +18,18 @@ body {
   font-family: ${({ theme }) => theme.fonts.family.primary}
 }
 h1, h2, h3, h4, h5, h6 {
-  font-family: ${({ theme }) => theme.fonts.family.secondary}
+  font-family: ${({ theme }) => theme.fonts.family.secondary};
+
+  margin: ${({ theme }) => theme.spacings.sizes.large} 0;
+}
+p {
+  margin: ${({ theme }) => theme.spacings.sizes.medium} 0;
+}
+ul, ol {
+  margin: ${({ theme }) => theme.spacings.sizes.medium} ;
+  padding: ${({ theme }) => theme.spacings.sizes.medium} ;
+}
+a {
+  color: ${({ theme }) => theme.colours.secondaryColor}
 }
 `
